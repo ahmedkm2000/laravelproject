44ca8cde5f35c26a91d8ec22411b5f254aa2d341
elkaoumiahmed <elkaoumiahmed>
2021-06-21 01:48:52 +0100

    laravel

---
+++ projet laravel_voyager/projetlaravel/app/Widgets/Filieres.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Widgets;
+
+use TCG\Voyager\Widgets\BaseDimmer;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+
+class Filieres extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Models\Filiere::count();
+        $string = 'Filieres';
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-study',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.post_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => 'Filieres',
+                'link' => route('voyager.filieres.index'),
+            ],
+            'image' => '/jane-carmona-8GVsvPuiLUA-unsplash.jpg',
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('Post'));
+    }
+}
